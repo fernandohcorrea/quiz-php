53e6fa568960bf7ea9ecd7dc0afbb8847e3edd34
fernandohcorrea <fernandohcorrea@gmail.com>
2017-02-22 00:17:56 +0000

    Diretórios de Questões

--- src/Config/Core.php
+++ src/Config/Core.php
@@ -1,5 +1,12 @@
 <?php
+
+// $dir = 'Lisura';
+// $dir = 'Ivan';
+// $dir = 'Adinan';
+// $dir = 'Fernando';
+$dir = 'SampleQuestions';
+
 return array(
-    'questions_dir' => realpath(__DIR__ . DIRECTORY_SEPARATOR . '../SampleQuestions'),
+    'questions_dir' => realpath(__DIR__ . DIRECTORY_SEPARATOR . "../Questions/{$dir}"),
     'questions_file_name' => 'questions'
 );
