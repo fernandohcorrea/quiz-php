935aeb791fe9f127020218c88473398bc2ed3c4e
fernandohcorrea <fernandohcorrea@gmail.com>
2017-02-22 02:09:38 +0000

    Pasta de Questões

---
+++ src/Questions/Adinan/questions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MODELO
+ *
+ * return array(
+ *      array( 
+ *          'question' => 'Avalie e identifique no código abaixo todos os padrões de projeto:',
+ *          'file_code_sample' => __DIR__.DIRECTORY_SEPARATOR.'CodeSample.php',
+ *          'multiple_choices' => array(
+ *              0 => 'AAAaa',
+ *              1 => 'AAAaa',
+ *              2 => 'AAAaa',
+ *              4 => 'AAAaa',
+ *              5 => 'AAAaa'
+ *          ),
+ *          'answer' => array(2)
+ *      ),
+ * );
+ */
+
+return array(
+    
+    array(
+        'question' => 'Question?',
+        //'file_code_sample' => __DIR__.DIRECTORY_SEPARATOR.'<file>.php',
+        'multiple_choices' => array(
+               0 => 'a?',
+               1 => 'b?',
+               2 => 'c?',
+               3 => 'd?',
+               4 => 'e?',
+        ),
+        'answer' => array(2)
+    ),
+    
+    
+);
